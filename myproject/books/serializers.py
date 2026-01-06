736dfcd707a72b40093e38bc2bed5b677df339d9
bazighaliminhas <bazighminhas1@gmail.com>
2026-01-06 18:58:36 +0500

    msg

--- myproject/books/serializers.py
+++ myproject/books/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Book, Author
+from categories.models import Category
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,12 +8,26 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
+    # Display names in API responses
     author_name = serializers.CharField(source='author.name', read_only=True)
-    category_name = serializers.SerializerMethodField()  # Optional: show category name
+    category_name = serializers.CharField(source='category.name', read_only=True, default=None)
+    
+    # Accept IDs in POST/PUT/PATCH requests
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(),
+        write_only=True
+    )
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        write_only=True,
+        allow_null=True,
+        required=False
+    )
 
     class Meta:
         model = Book
-        fields = ['id', 'title',  'author_name', 'category_name', 'price']
+        fields = ['id', 'title', 'author', 'author_name', 'category', 'category_name', 'price']
+
 
     def get_category_name(self, obj):
         return obj.category.name if obj.category else None
